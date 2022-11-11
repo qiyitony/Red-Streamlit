5b597620f3cffe1e3880ba98a3613a4dd17934dc
Hongtao Zou <94774666+qiyitony@users.noreply.github.com>
2022-11-11 13:31:11 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -98,7 +98,7 @@ if check_password():
         with st2:
             st.metric(label="Accuracy", value="89%")
         st.button('view the report')
-        st.button('Approve and share the report',on_click= routing_three())
+        st.button('Approve and share the report')
         st.button('Reject the report')
 
 
